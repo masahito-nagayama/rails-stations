1e7b8a5bbfd72ed8abe6f9a7da2803cd3c57be50
masahito-nagayama <me18sf67m@gmail.com>
2021-09-13 21:14:37 +0900

    fix

--- db/migrate/20210907094945_create_movies.rb
+++ db/migrate/20210907094945_create_movies.rb
@@ -2,7 +2,10 @@ class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
       t.string "name"
-      t.string "image"
+      t.string "year"
+      t.text "description"
+      t.string "image_url"
+      t.integer "is_showing"
       t.timestamps
     end
   end
